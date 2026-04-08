f2333df03bbced13f8b4030fae8d606753f255d3
SANDRICKSON <sandroqatamadze802@gmail.com>
2026-04-08 05:55:09 -0700

    Tailwind package

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,35 @@
 import React from 'react';
-import {Button, FlexAlignType, StyleSheet, Text, View} from 'react-native';
-import {Space_Separator} from "json5/lib/unicode";
-import styled from "styled-components/native";
+import {View, StyleSheet} from 'react-native';
+import styled from 'styled-components/native';
 
+// Optional: styled-components-ს შეუძლია background + padding
 const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
     background-color: navy;
-`
-
-const HelloText = styled.Text`
-   font-size: 20px;
-    color:black;
-    margin: 10px;
-`
+    padding: 16px;
+`;
 
 export default function Counter() {
-
-
-
     return (
+        // ამ შემთხვევაში შეგიძლია გამოიყენო უბრალოდ styled-components
         <Container>
-            <HelloText>Hello World</HelloText>
-
+            {/* აქ შეგიძლია HelloText ან სხვა JSX */}
         </Container>
+
+        // ან მხოლოდ StyleSheet
+        /*
+        <View style={[styles1['bg-blue-500'], styles1['p-4']]}></View>
+        */
     );
 }
 
-
+const styles1 = StyleSheet.create({
+    "bg-blue-500": {
+        backgroundColor: "red", // შენ შეგიძლია შეცვალო აქ navy ან სხვა ფერი
+    },
+    "p-4": {
+        padding: 16,
+    },
+});
