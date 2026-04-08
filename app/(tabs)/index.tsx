c09d10c81174e85f106743fb6000cc321a94c73f
SANDRICKSON <sandroqatamadze802@gmail.com>
2026-04-08 04:26:34 -0700

    Hello world

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,16 @@
+import { Text } from 'react-native';
+
+import { View } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View
+    style={{
+      flex:1,
+      justifyContent:'center',
+      alignItems:'center',
+    }}>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
