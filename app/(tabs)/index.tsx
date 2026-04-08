5570e78ab1c21168d9a91d0256c64f8b8117ef0d
SANDRICKSON <sandroqatamadze802@gmail.com>
2026-04-08 05:47:33 -0700

    styled components library

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,40 +1,31 @@
 import React from 'react';
 import {Button, FlexAlignType, StyleSheet, Text, View} from 'react-native';
 import {Space_Separator} from "json5/lib/unicode";
+import styled from "styled-components/native";
 
+const Container = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: navy;
+`
+
+const HelloText = styled.Text`
+   font-size: 20px;
+    color:black;
+    margin: 10px;
+`
 
 export default function Counter() {
-    const [count, setCount] = React.useState<number>(0);
 
-
-    const increment = () => setCount(count + 1);
-    const decrement = () => setCount(count - 1);
 
 
     return (
-        <View style={styles.container}>
-            <Text>Counter App</Text>
-            <Text style={styles.text}>{count}</Text>
-            <Button title="Increase" onPress={increment}></Button>
-            <Button title="Decrease" onPress={decrement}></Button>
-        </View>
+        <Container>
+            <HelloText>Hello World</HelloText>
+
+        </Container>
     );
 }
 
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center' as FlexAlignType,
-        justifyContent: 'center',
-    },
-
-    header: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
-    text: {
-        fontSize: 25,
-        fontWeight: 'bold',
-    }
-})
