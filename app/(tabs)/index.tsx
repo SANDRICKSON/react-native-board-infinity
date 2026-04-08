043ed153413d32a04c6e3bd1d972e91fd167e502
SANDRICKSON <sandroqatamadze802@gmail.com>
2026-04-08 11:55:29 -0700

    Responsive UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,71 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import styled from 'styled-components/native';
+import {Button, FlatList, FlexAlignType, StyleSheet, Text, useWindowDimensions, View} from 'react-native';
+import {Image} from "expo-image";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
 
-// Optional: styled-components-ს შეუძლია background + padding
-const Container = styled.View`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: navy;
-    padding: 16px;
-`;
 
-export default function Counter() {
-    return (
-        // ამ შემთხვევაში შეგიძლია გამოიყენო უბრალოდ styled-components
-        <Container>
-            {/* აქ შეგიძლია HelloText ან სხვა JSX */}
-        </Container>
-
-        // ან მხოლოდ StyleSheet
-        /*
-        <View style={[styles1['bg-blue-500'], styles1['p-4']]}></View>
-        */
-    );
+interface Product {
+    id: number;
+    name: string;
+    image: string;
 }
 
-const styles1 = StyleSheet.create({
-    "bg-blue-500": {
-        backgroundColor: "red", // შენ შეგიძლია შეცვალო აქ navy ან სხვა ფერი
+const products: Product[] = [
+    {
+        id: 1,
+        name: "Apple iPhone 15",
+        image: "https://images.pexels.com/photos/5081929/pexels-photo-5081929.jpeg",
     },
-    "p-4": {
-        padding: 16,
+    {
+        id: 2,
+        name: "Nike Air Max Sneakers",
+        image: "https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg",
     },
-});
+    {
+        id: 3,
+        name: "Sony WH-1000XM5 Headphones",
+        image: "https://images.pexels.com/photos/3394653/pexels-photo-3394653.jpeg",
+    },
+    {
+        id: 4,
+        name: "MacBook Pro 16\"",
+        image: "https://images.pexels.com/photos/18105/pexels-photo.jpg",
+    },
+    {
+        id: 5,
+        name: "Canon EOS R5 Camera",
+        image: "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg",
+    },
+];
+
+
+
+
+export default function Index() {
+    const window = useWindowDimensions();
+    const numColumns = window.width > 600 ? 3 : 2;
+
+    const renderItem = ({ item }: { item: Product }) => {
+        return (
+            <View style={{ flex: 1, margin: 5 }}>
+                <Text>{item.name}</Text>
+                <Image
+                    source={{ uri: item.image }}
+                    style={{ width: '100%', height: 100 }}
+                />
+            </View>
+        );
+    };
+
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <FlatList
+                data={products}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={numColumns}
+                contentContainerStyle={{ padding: 10 }}
+            />
+        </GestureHandlerRootView>
+    );
+}
