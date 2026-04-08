88fae4c66df877c1bbb7b2496549a922f2bf2e64
SANDRICKSON <sandroqatamadze802@gmail.com>
2026-04-08 05:22:07 -0700

    JSX Basics

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,41 @@
-import { Component, ReactNode } from 'react';
-import { Text } from 'react-native';
+import React from 'react';
+import { FlexAlignType, StyleSheet, Text, View } from 'react-native';
 
-import { View } from 'react-native';
+interface Auth {
+  isLoggedIn: boolean;
 
-class HelloWorld extends Component{
-  render(){
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello World</Text>
-      </View>
-    )
-  }
 }
 
-export default HelloWorld;
+const Greeting: React.FC<Auth> = ({ isLoggedIn }) => {
+  return isLoggedIn ? <Text>Welcome back!</Text> : <Text>Please sign up.</Text>
+}
+const Header: React.FC = () => {
+  return <Text style={styles.header}>Header</Text>
+}
+export default function HomeScreen() {
+  const items = ['Apple', 'Banana', 'Cherry'];
+
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <Text>Hello World</Text>
+      {items.map((item, index) => (<Text key={index}>{item}</Text>))}
+      <Greeting isLoggedIn={true} />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center' as FlexAlignType,
+    justifyContent: 'center',
+  },
+
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  }
+})
